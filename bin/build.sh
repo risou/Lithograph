e3d4b1eecc9eaea56626329716934c5de12dc2ed
risou <risou.f@gmail.com>
2019-08-28 02:15:23 +0900

    Fix path in shell

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-ls -A articles/*.md | sort -r | xargs basename | xargs -I {} perl6 ~/src/github.com/risou/Lithograph/bin/lithograph build article {}
-perl6 ~/src/github.com/risou/Lithograph/bin/lithograph build index
-perl6 ~/src/github.com/risou/Lithograph/bin/lithograph build list
-perl6 ~/src/github.com/risou/Lithograph/bin/lithograph build static
+export PERL6LIB=Lithograph/lib
+ls -A articles/*.md | sort -r | xargs -n1 basename | xargs -I {} perl6 Lithograph/bin/lithograph build article {}
+perl6 Lithograph/bin/lithograph build index
+perl6 Lithograph/bin/lithograph build list
+perl6 Lithograph/bin/lithograph build static
 
